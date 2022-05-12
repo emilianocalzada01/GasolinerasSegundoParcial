bbd01c1194cb32d5daeb24740985af99d2e30d4f
emiliano <emilianocalzada01@outlook.com>
2022-05-11 20:15:22 -0500

    Add project files.

---
+++ GasolinerasSegundoParcial/GasolinerasSegundoParcial/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using GasolinerasSegundoParcial.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace GasolinerasSegundoParcial.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
